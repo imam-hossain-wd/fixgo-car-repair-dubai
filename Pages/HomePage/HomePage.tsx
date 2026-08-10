3e832fa2604e3aec816fafc1decaef51716d5af7
imam-hossain-wd <zunayedemon6t9@gmail.com>
2026-08-10 23:55:45 +0600

    added services section in homepage

--- Pages/HomePage/HomePage.tsx
+++ Pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/View/Hero/Hero";
 import HowItWorks from "@/components/View/HowItWorks/HowItWorks";
+import Services from "@/components/View/Services/Services";
 
 
 export default function HomePage() {
@@ -7,6 +8,7 @@ export default function HomePage() {
         <div>
             <Hero />
             <HowItWorks />
+            <Services />
         </div>
     )
 }
